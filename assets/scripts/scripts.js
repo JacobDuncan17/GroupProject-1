c8075164c952cd1c9b44d0c5c4b687703f90b829
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-01-26 19:22:16 -0700

    Add complete activity functionality

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -17,16 +17,18 @@ const tomorrow = today.add(1, "day")
 const dayAfterTomorrow = today.add(2, "day")
 
 $(function () {
-    $("#today").text(today.format("MMM Do, YYYY")) 
-    $("#todayDay").text(today.format("dddd")) 
-    $("#tomorrow").text(tomorrow.format("MMM Do, YYYY")) 
-    $("#tomorrowDay").text(tomorrow.format("dddd")) 
-    $("#dayAfterTomorrow").text(dayAfterTomorrow.format("MMM Do, YYYY")) 
-    $("#dayAfterTomorrowDay").text(dayAfterTomorrow.format("dddd")) 
- });
- 
+    $("#today").text(today.format("MMM Do, YYYY"))
+    $("#todayDay").text(today.format("dddd"))
+    $("#tomorrow").text(tomorrow.format("MMM Do, YYYY"))
+    $("#tomorrowDay").text(tomorrow.format("dddd"))
+    $("#dayAfterTomorrow").text(dayAfterTomorrow.format("MMM Do, YYYY"))
+    $("#dayAfterTomorrowDay").text(dayAfterTomorrow.format("dddd"))
+});
+
+completeButton = $("#complete-activity");
+completeButton.hide();
 const generateActivity = document.querySelector("#generateActivityBtn");
-generateActivity.onclick = ()=>{
+generateActivity.onclick = () => {
     activity.style.display = "flex";
     generateActivity.style.display = "none"
     bored();
@@ -35,23 +37,27 @@ const boredApi = 'http://www.boredapi.com/api/activity/'
 const activity = document.querySelector("#activityText1");
 function bored() {
     fetch(boredApi)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                let boredApiData = data;
-                const dataString = JSON.stringify(boredApiData.activity);
-                activity.textContent = boredApiData.activity;
-            })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let boredApiData = data;
+            const dataString = JSON.stringify(boredApiData.activity);
+            activity.textContent = boredApiData.activity;
+            completeButton.show();
+        })
 }
 
+// Complete Activity
+completeButton.click(function () {
+    activity.style.backgroundColor = "aquamarine";
+    activity.style.color = "#3a3335";
+    completeButton.hide();
+});
 
-
-
-    
-    $("#today").text(today.format("MMM Do, YYYY"))
-    $("#todayDay").text(today.format("dddd"))
-    $("#tomorrow").text(tomorrow.format("MMM Do, YYYY"))
-    $("#tomorrowDay").text(tomorrow.format("dddd"))
-    $("#dayAfterTomorrow").text(dayAfterTomorrow.format("MMM Do, YYYY"))
-    $("#dayAfterTomorrowDay").text(dayAfterTomorrow.format("dddd"))
+$("#today").text(today.format("MMM Do, YYYY"))
+$("#todayDay").text(today.format("dddd"))
+$("#tomorrow").text(tomorrow.format("MMM Do, YYYY"))
+$("#tomorrowDay").text(tomorrow.format("dddd"))
+$("#dayAfterTomorrow").text(dayAfterTomorrow.format("MMM Do, YYYY"))
+$("#dayAfterTomorrowDay").text(dayAfterTomorrow.format("dddd"))
