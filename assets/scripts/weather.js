52804cb3bfb2ac23f5a3d55c3370c489f13e1c43
alfint <alexandreaflint1111@gmail.com>
2023-01-30 19:06:07 -0700

    adding local storage functionality for weather

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -1,5 +1,5 @@
 // Multiple API keys in the event a too many request errors (uncomment when wanting to make request)
-// const API_KEY = "gRRRRVjxrc405mmY6eWFvSffWXP9BewE"
+const API_KEY = "gRRRRVjxrc405mmY6eWFvSffWXP9BewE"
 // const API_KEY = "jjzTJr7q0YhPamWmrzRNvv4qzaqZ80vj"
 // const API_KEY = "mecIrjhSkzaHiivR7ArVZyTWWxEgO9VD"
 // const API_KEY = "tcwEB37u1ckcavIFDQnnU94EpfxREhpM"
@@ -15,37 +15,26 @@ const precipitationTypes = {
 }
 
 // Gets the average value of an array of numbers
-function getAverage(arr){
+function getAverage(arr) {
   let total = 0
   arr.forEach(e => {
-    total+=e
+    total += e
   })
-  return Math.round(total/arr.length)
+  return Math.round(total / arr.length)
 }
 
 // Gets the mode value of an array of any type
-function getMostOccurringValue(arr){
+function getMostOccurringValue(arr) {
   let obj = {}
   arr.forEach(e => {
-      if(obj[e] == null){
-          obj[e] = 1
-      } else {
-          obj[e] += 1
-      }
+    if (obj[e] == null) {
+      obj[e] = 1
+    } else {
+      obj[e] += 1
+    }
   })
-  return Object.keys(obj).reduce((a,b) => obj[a] > obj[b] ? a : b)
+  return Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b)
 }
-
-// IF geolocation is successful, save lat/long to local storage
-const successCallback = (position) => {
-  localStorage.setItem("lat", position.coords.latitude);
-  localStorage.setItem("long", position.coords.longitude);
-};
-
-// IF geolocation retrieval is not successful, log error as to why it was not successful
-const errorCallback = (error) => {
-  console.log(error);
-};
 
 // DEFINE method to fetch data from API
 async function fetchTimelines(lat, long, times_steps = "1d", start_time, end_time) {
@@ -67,31 +56,31 @@ async function getWeatherData(lat, long) {
   var responses = []
 
   // ESTABLISH start time depending on the index
-  for(var i = 0; i < 3; i++){
-    if(i==0){
+  for (var i = 0; i < 3; i++) {
+    if (i == 0) {
       start_time = today.toISOString()
       end_time = today.endOf('day').toISOString()
-    } else if(i==1){
+    } else if (i == 1) {
       start_time = tomorrow.startOf('day').toISOString()
       end_time = tomorrow.endOf('day').toISOString()
     } else {
       start_time = dayAfterTomorrow.startOf('day').toISOString()
       end_time = dayAfterTomorrow.endOf('day').toISOString()
     }
-    // TODO - uncomment when wanting to use actual api
-    // response = await fetchTimelines(lat, long, "1h", start_time, end_time)
-    // responses.push(response)
+
+    response = await fetchTimelines(lat, long, "1h", start_time, end_time)
+    responses.push(response)
   }
-  // TODO - change to responses when not wanting to mock
-  return mock_response
+
+  return responses
 }
 
 // Parse weather data
-function parseRawWeatherData(weather_data){
+function parseRawWeatherData(weather_data) {
   let parsed_response = {}
 
   weather_data.forEach((e, index) => {
-    
+
     let temperatures = []
     let humidity = []
     let precipitationProbability = []
@@ -115,15 +104,15 @@ function parseRawWeatherData(weather_data){
 }
 
 // UPDATE page with our parsed weather data
-function updatePageWithWeatherData(parsed_weather_data){
+function updatePageWithWeatherData(parsed_weather_data) {
   let jqueryId
   Object.keys(parsed_weather_data).forEach(e => {
-    if(e==0){
-      jqueryId="todayWeather"
-    } else if(e==1){
-      jqueryId="tomorrowWeather"
+    if (e == 0) {
+      jqueryId = "todayWeather"
+    } else if (e == 1) {
+      jqueryId = "tomorrowWeather"
     } else {
-      jqueryId="dayAfterTomorrowWeather"
+      jqueryId = "dayAfterTomorrowWeather"
     }
 
     $(`#${jqueryId}`).append(`
@@ -135,7 +124,7 @@ function updatePageWithWeatherData(parsed_weather_data){
       </div>
     `)
 
-    if(parsed_weather_data[e].precipitationProbability > 0){
+    if (parsed_weather_data[e].precipitationProbability > 0) {
       $(`#${jqueryId}`).append(`
         <div class="grid grid-cols-1 flex justify-items-center ">
           <div>${parsed_weather_data[e].precipitationProbability}% chance of ${precipitationTypes[parsed_weather_data[e].precipitationType]}</div>
@@ -151,17 +140,45 @@ function updatePageWithWeatherData(parsed_weather_data){
   })
 }
 
+
+
+
 //RUN on page load
 $(async function () {
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-  var lat = localStorage.getItem("lat") 
-  var long = localStorage.getItem("long")
-  if(lat == null &&  long == null){
-    alert("For this application to successfully load proper weather data, please enable tracking services in your browser.")
+
+  // Async function to get coordinates
+  const getCoords = async () => {
+    try {
+      const pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+
+      return {
+        lat: pos.coords.latitude,
+        long: pos.coords.longitude,
+      };
+    } catch {
+      return {
+        lat: null,
+        long: null
+      };
+    }
+  };
+
+  // Must await to see if we were able to get coordinates
+  const coords = await getCoords();
+
+  if (coords.lat == null || coords.long == undefined) {
+    $("#toast").removeClass("hide")
   } else {
-    const weather_data = await getWeatherData(lat, long)
+    let weather_data
+    if (localStorage.getItem(today.format("MM-DD-YYYY")) == null) {
+      weather_data = await getWeatherData(coords.lat, coords.long)
+      localStorage.setItem(today.format("MM-DD-YYYY"), JSON.stringify(weather_data))
+    } else {
+      weather_data = JSON.parse(localStorage.getItem(today.format("MM-DD-YYYY")))
+    }
     const parsed_weather_data = parseRawWeatherData(weather_data)
     updatePageWithWeatherData(parsed_weather_data)
   }
 });
-
